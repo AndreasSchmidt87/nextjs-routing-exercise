9d64bb1cdebfccdaefe944c62c634de3a7bdf4e1
AndySch666 <schmidt.hof@web.de>
2022-09-07 11:19:58 +0200

    add links

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
+import Link from 'next/link';
 import styled from 'styled-components';
 
 export default function Header() {
   return (
     <AppHeader>
       <Nav>
-        <a>Home</a>
-        <a>Product</a>
-        <a>Team</a>
-        <a>Account</a>
+        <Link href="/"><a>Home</a></Link>
+        <Link href="/product"><a>Product</a></Link>
+        <Link href="/team"><a>Team</a></Link>
+        <Link href="/account"><a>Account</a></Link>
       </Nav>
     </AppHeader>
   );
